0ccd7a6d7aab126b9ca9973e0d75bed51d9e19eb
joe Defendre <jchacker5@gmail.com>
2023-03-15 13:42:52 -0400

    added a simple front end

--- whisper_transcriber.py
+++ whisper_transcriber.py
@@ -1,5 +1,33 @@
+import os
+import tempfile
+from flask import Flask, request, redirect, url_for, render_template
 import whisper
 
-model = whisper.load_model("base")
-result = model.transcribe("audio.m4a")
-print(result["text"])
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        file = request.files["file"]
+        if file:
+            # Save the uploaded file to a temporary directory
+            filename = os.path.join(tempfile.gettempdir(), file.filename)
+            file.save(filename)
+            # Load the transcription model and transcribe the audio file
+            model = whisper.load_model("base")
+            result = model.transcribe(filename)
+            # Render the transcription result on a separate page
+            return redirect(url_for("result", text=result["text"]))
+    return render_template("index.html")
+
+
+@app.route("/result")
+def result():
+    text = request.args.get("text")
+    return render_template("result.html", text=text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
